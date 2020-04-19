a8f6fa1b50d242679dfa0bf0ef587563012a05ee
Amit Yadav <amitsyadav1999@gmail.com>
2020-04-19 17:39:14 +0530

    completed till posting a form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -10,4 +11,17 @@ def post_list(request):
 
 def post_details(request,  pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_details", {"posts":post})
+    return render(request, "blog/post_details.html", {"posts":post})
+
+def post_new(request):
+    # print(request.method) on first reload method is by default always get
+    if request.method=="POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) #saving form but not saving it in database; .save return a model
+            post.author = request.user
+            post.published_date =  timezone.now()
+            post.save()
+            return redirect('post_details', pk=post.pk)
+    form = PostForm()
+    return render(request, "blog/post_edit.html", {"form":form})
